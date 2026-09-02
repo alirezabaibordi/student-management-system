f6d364fa0c66a07319907fe965bc91b1ae74f0b1
Alireza Baibordi <alirezabaibordi@gmail.com>
2026-09-02 22:14:34 +0330

    Add student model class

---
+++ src/Project/co/Student.java
@@ -0,0 +1,43 @@
+package Project.co;
+
+public class Student {
+  private   String name;
+    private int id;
+    private String major;
+    private int age;
+    private int gpa;
+
+    public void student() {
+
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setMajor(String major) {
+        this.major = major;
+    }
+    public String getMajor() {
+        return major;
+    }
+    public void setGpa(int gpa) {
+        this.name = name;
+    }
+    public int getGpa() {
+        return gpa;
+    }
+
+
+
+}
